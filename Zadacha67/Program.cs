1179995a850ee10e891807b228f32ab522cd086a
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-25 00:58:22 +0300

    add Zadacha67

---
+++ Zadacha67/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+void SumFunt(int num, int sum = 0)
+{
+    Console.WriteLine($"{num} {sum}");
+    if (num == 0)
+    {
+        Console.Write($"{sum}");
+        return;
+    }
+
+    sum += num % 10;
+    num = num / 10;
+
+    SumFunt(num, sum);
+}
+SumFunt(123456);
